d3c64df1ade0c3d651fbf43de2a360277346b29e
ncs01060 <ncs01060>
2023-07-09 12:46:58 +0900

    first commit

---
+++ backend/view/js/router.js
@@ -0,0 +1,13 @@
+function gyeyoungdok(){
+    window.location.href = "friend/gyeyoungdok.html"
+}
+
+function leesuckhyun(){
+    window.location.href = "friend/leesuckhyun.html"
+}
+function jungyoung(){
+    window.location.href = "friend/jungyoung.html"
+}
+function dogun(){
+    window.location.href = "friend/dogun.html"
+}
